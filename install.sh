871da33a26a4f2ed7e316aedea81a8923d05122b
mdelage <mdelage@student.42.fr>
2015-06-21 22:03:07 +0200

    corret minor bug

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # vim config
-rm -rf $HOME/.vimrc $HOME/.vim
-git clone git@github.com:Geam/config_vim.git $HOME/.vim
-if [[ "$?" -eq 0 ]]; then
-    cd $HOME/.vim
-    git submodule init && git submodule update
-    mkdir $HOME/.vim/tmp
-fi
+#rm -rf $HOME/.vimrc $HOME/.vim
+#git clone git@github.com:Geam/config_vim.git $HOME/.vim
+#if [[ "$?" -eq 0 ]]; then
+#    cd $HOME/.vim
+#    git submodule init && git submodule update
+#    mkdir $HOME/.vim/tmp
+#fi
 
 if [[ `uname` = "Darwin" ]]; then
     # add exclude file
@@ -29,6 +29,6 @@ if [[ `uname` = "Darwin" ]]; then
     ln -s $PERS_PATH/42_related/backup.sh $PERS_PATH/scripts/backup
 
     # link goindre to Music
-    rm -rf Music
+    rm -r "$HOME/Music"
     ln -s /nsf/sgoinfre/goinfre/Music $HOME/Music
 fi
