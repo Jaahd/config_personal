a8ff7063fdf934fe0e29f9e3357f54ad048f2e67
mdelage <mdelage@student.42.fr>
2015-06-21 22:05:12 +0200

    sigh, forget comment

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # vim config
-#rm -rf $HOME/.vimrc $HOME/.vim
-#git clone git@github.com:Geam/config_vim.git $HOME/.vim
-#if [[ "$?" -eq 0 ]]; then
-#    cd $HOME/.vim
-#    git submodule init && git submodule update
-#    mkdir $HOME/.vim/tmp
-#fi
+rm -rf $HOME/.vimrc $HOME/.vim
+git clone git@github.com:Geam/config_vim.git $HOME/.vim
+if [[ "$?" -eq 0 ]]; then
+    cd $HOME/.vim
+    git submodule init && git submodule update
+    mkdir $HOME/.vim/tmp
+fi
 
 if [[ `uname` = "Darwin" ]]; then
     # add exclude file
