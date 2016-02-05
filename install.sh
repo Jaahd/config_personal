a080b32a9a5dbb966e3c5c16f5363225452c410a
Marc DELAGE <mdelage@student.42.fr>
2016-02-05 14:00:57 +0100

    install exa and install font for neovim

--- install.sh
+++ install.sh
@@ -33,4 +33,20 @@ if [[ -n $SCHOOL42 ]]; then
     if [[ ! -e "$PERS_PATH/scripts" ]]; then
         mkdir "$PERS_PATH/scripts"
     fi
+    $PERS_PATH/osx
+    mkdir $HOME/bin
+
+    # add exa
+    curl -0 https://github.com/ogham/exa/releases/download/v0.4.0/exa-osx-x86_64.zip > $HOME/exa-osx-x86_64.zip
+    unzip $HOME/exa-osx-x86_64.zip
+    mv exa-osx-x86_64 $HOME/bin/exa
+    rm exa-osx-x86_64.zip
+
+    # add font for osx
+    cd
+    git clone https://github.com/powerline/fonts temp_fonts
+    cd temp_fonts
+    mkdir ~/Library/Fonts
+    ./install.sh
+    open /Applications/Font\ Book.app
 fi
